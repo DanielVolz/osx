ad162a1cea20dc1cf87eac174f15744a03c2edbc
empty23 <volz.danie@gmail.com>
2015-09-29 05:52:22 +0200

    asd

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,49 +9,49 @@ sudo bash -c "printf '/usr/local/bin/bash\n' >> /etc/shells"
 # Bash Completion
 chsh -s /usr/local/bin/bash
 
-# rbenv
-export PATH="/usr/local/bin:$PATH"
-eval "$(rbenv init -)"
-for ruby in $MRI $JRUBY; do
-  rbenv install $ruby
-done
-rbenv global $MRI
+# # rbenv
+# export PATH="/usr/local/bin:$PATH"
+# eval "$(rbenv init -)"
+# for ruby in $MRI $JRUBY; do
+#   rbenv install $ruby
+# done
+# rbenv global $MRI
+#
+# # # Ruby
+# printf "%s\n" "---" > "$HOME/.gemrc"
+# printf "%s\n" "gem: --no-document" >> "$HOME/.gemrc"
+# git clone git://github.com/bkuhlmann/ruby_setup.git
+# (
+#   cd ruby_setup
+#   git checkout $REPO_RUBY_SETUP
+#   for ruby in $MRI $JRUBY; do
+#     rbenv shell $ruby
+#     ./run.sh i
+#   done
+# )
+# rm -rf ruby_setup
 
-# Ruby
-printf "%s\n" "---" > "$HOME/.gemrc"
-printf "%s\n" "gem: --no-document" >> "$HOME/.gemrc"
-git clone git://github.com/bkuhlmann/ruby_setup.git
-(
-  cd ruby_setup
-  git checkout $REPO_RUBY_SETUP
-  for ruby in $MRI $JRUBY; do
-    rbenv shell $ruby
-    ./run.sh i
-  done
-)
-rm -rf ruby_setup
-
-# Go
-install_git_project "git://github.com/bkuhlmann/go_setup.git" $REPO_GO_SETUP "go_setup" "./run.sh i"
-
-# NPM
-install_git_project "git://github.com/bkuhlmann/npm_setup.git" $REPO_NPM_SETUP "npm_setup" "./run.sh i"
-
-# PostgreSQL
-initdb /usr/local/var/postgres -E utf8
-launch_process "/usr/local/opt/postgresql/homebrew.mxcl.postgresql.plist"
-
-# Nginx
-launch_process "/usr/local/opt/nginx/homebrew.mxcl.nginx.plist"
-
-# Memcached
-launch_process "/usr/local/opt/memcached/homebrew.mxcl.memcached.plist"
-
-# Redis
-launch_process "/usr/local/opt/redis/homebrew.mxcl.redis.plist"
-
-# Pow
-curl get.pow.cx | sh
+# # Go
+# install_git_project "git://github.com/bkuhlmann/go_setup.git" $REPO_GO_SETUP "go_setup" "./run.sh i"
+#
+# # NPM
+# install_git_project "git://github.com/bkuhlmann/npm_setup.git" $REPO_NPM_SETUP "npm_setup" "./run.sh i"
+#
+# # PostgreSQL
+# initdb /usr/local/var/postgres -E utf8
+# launch_process "/usr/local/opt/postgresql/homebrew.mxcl.postgresql.plist"
+#
+# # Nginx
+# launch_process "/usr/local/opt/nginx/homebrew.mxcl.nginx.plist"
+#
+# # Memcached
+# launch_process "/usr/local/opt/memcached/homebrew.mxcl.memcached.plist"
+#
+# # Redis
+# launch_process "/usr/local/opt/redis/homebrew.mxcl.redis.plist"
+#
+# # Pow
+# curl get.pow.cx | sh
 
 # Google Chrome Ember Inspector
 (
